e3ac8647cacd0bc95363eb9460b7aaeb32176aab
pdecembre <pdecembre@gmail.com>
2017-01-15 11:27:07 -0500

    adding login info

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
    moduleId: module.id,
@@ -8,7 +11,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authSvc: AuthService, private router: Router) {
+  }
+
+  login(){
+
+  }
 
   ngOnInit() {
   }
