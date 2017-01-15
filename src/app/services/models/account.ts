f019e20da0af4ef7e37eede108927156f484c4ad
pdecembre <pdecembre@gmail.com>
2017-01-15 16:35:02 +0000

    commit new file

---
+++ src/app/services/models/account.ts
@@ -0,0 +1,8 @@
+export class Login
+{
+    constructor(public email: string, public password:string){}
+}
+
+export class Register{
+    constructor(public name: string, public email: string, public password:string){}
+}
